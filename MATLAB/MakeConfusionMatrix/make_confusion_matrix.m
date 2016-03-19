f5603ea2dfd3cc7445368341a3558f0dd683f412
xcpeng <jackjack.peng@gmail.com>
2016-03-19 15:44:45 -0400

    Update make_confusion_matrix.m

--- MATLAB/MakeConfusionMatrix/make_confusion_matrix.m
+++ MATLAB/MakeConfusionMatrix/make_confusion_matrix.m
@@ -4,8 +4,14 @@
 
 %We use plotconfusion() to make the confusion matrix, C is the 
 %final confusion matrix
-[C, index] = plotconfusion(gt_labels, labels);
-
+%[C, index] = plotconfusion(gt_labels, labels);
+[C, index] = confusionmat(gt_labels, labels);
+	h = imagesc(C);
+	colorbar;
+	p = get(h);	
+	data = p.CData;
+	data = uint8(data);
+	imwrite(data,[dir '/confusion.jpg' ],'jpg');
 imagesc(C); 
 % imagesc() will scale image data to the full range of current 
 % colormap
